15624ce83e78fb8ea0146649e98b3c6f99cb796a
halsschmerzen <bastiansteampl@gmail.com>
2023-09-05 18:53:54 +0200

    Working buttons and logic - Float needs implemen.

--- script.js
+++ script.js
@@ -28,4 +28,47 @@ function operate(operator, val1, val2) {
       return "Invalid operator";
   }
 }
+// Add a function that allows EventListening on Buttons and that checks what button was clicked.
+// I KNOW THIS IS A HUGE MESS BUT I AM SO F*CKING HAPPY IT WORKS :DDDD!!!
+function buttonParser() {
+  let value1 = "";
+  let value2 = "";
+  let currentOperator = "";
+  const calculatorButtons = document.querySelectorAll("#buttonfield button");
+  calculatorButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      // If the button that was clicked is a number -> add that to our first value
+      if (!isNaN(button.textContent) && currentOperator == "") {
+        value1 += button.textContent;
+        value1 = parseFloat(value1);
+        setDisplay(value1);
+      } else if (isNaN(button.textContent) && button.textContent != ".") {
+        if (button.textContent != "=" && button.textContent != "C") {
+          currentOperator = button.textContent;
+        } else if (button.textContent == "C") {
+          value1 = "";
+          value2 = "";
+          currentOperator = "";
+          setDisplay(0);
+        } 
+      }
+      if (!isNaN(button.textContent) && currentOperator != "") {
+        value2 += button.textContent;
+        value2 = parseFloat(value2);
+        setDisplay(value2);
+      }
+      if (currentOperator != "" && button.textContent == "=") {
+        setDisplay(operate(currentOperator, value1, value2));
+        value1 = operate(currentOperator, value1, value2);
+        value2 = "";
+      }
+    });
+  });
+}
 
+function setDisplay(value) {
+  let currentDisplay = document.querySelector("#textcontent");
+  currentDisplay.textContent = value;
+}
+
+buttonParser();
